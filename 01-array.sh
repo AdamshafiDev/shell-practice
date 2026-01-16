d2a897e5949b6d9e8fdc5d487bbb5fe7f494cb9d
adamshafi <adamshafi0001.com>
2026-01-16 12:17:37 +0530

    hello

--- 01-array.sh
+++ 01-array.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+firstnumber=100
+secondnumber=200
+
+TIMESTAMP=$(date)
+sum=$(($firstnumber+$secondnumber))
+
+echo "sum of number: $sum"
+
 movies=("Bahubali" "HIT3" "Hero" "raju")
 
 echo "enater first movie ${movies[0]}"
@@ -8,5 +16,7 @@ echo "enater second movie ${movies[1]}"
 echo "enater third movie ${movies[2]}"
 
 echo "all movies: ${movies[@]}"
+
+
 
 
