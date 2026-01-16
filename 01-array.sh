50dc865e90692edeae2627a5fc51283e6af3a4c6
adamshafi <adamshafi0001.com>
2026-01-16 12:10:52 +0530

    hello

--- 01-array.sh
+++ 01-array.sh
@@ -2,10 +2,10 @@
 
 movies=("Bahubali" "HIT3" "Hero" "raju")
 
-echo "enater second movie ${movies[0]}"
+echo "enater first movie ${movies[0]}"
 
 echo "enater second movie ${movies[1]}"
-echo "enater second movie ${movies[2]}"
+echo "enater third movie ${movies[2]}"
 
 echo "all movies: ${movies[@]}"
 
