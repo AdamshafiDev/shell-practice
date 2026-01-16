16cbe2d3c6054a77c4d8afbae195a5ec360e7839
adamshafi <adamshafi0001.com>
2026-01-16 11:50:30 +0530

    hello

---
+++ 01-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+movies("Bahubali" "HIT3" "Hero" "raju")
+
+echo "enater second movie $(movies[0])"
+
+echo "enater second movie $(movies[1])"
+
+echo "all movies: $(movies[@])"
+
+
--- 03-test-pin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Enter pin number:"
-
-read -s PIN
-
-echo "the pin number is:: $PIN"
-
-
