351dcfeb48f11d012a771bf5862b0c66107902ad
adamshafi <adamshafi0001.com>
2026-01-16 11:59:42 +0530

    hello

--- 01-array.sh
+++ 01-array.sh
@@ -5,6 +5,7 @@ movies=("Bahubali" "HIT3" "Hero" "raju")
 echo "enater second movie ${movies[0]}"
 
 echo "enater second movie ${movies[1]}"
+echo "enater second movie ${movies[2]}"
 
 echo "all movies: ${movies[@]}"
 
