f76c9b058fbe2a4a0d4c8573579dd69e195242d7
adamshafi <adamshafi0001.com>
2026-01-17 20:00:06 +0530

    hello

---
+++ 02-array2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+number1=100
+number2=300
+number3=400
+
+echo "enter first number::$number1"
+echo "enter first number::$number2"
+echo "enter first number::$number3"
+ 
+ sum=$(($number1+$number2+$number3))
+
+ echo "sum of number is::$sum"
+
+ TIMESTAMP=$(date)
+
+ echo "the scripting exected at::$TIMESTAMP"
+
+ 
