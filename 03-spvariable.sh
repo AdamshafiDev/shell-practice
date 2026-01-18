cb5cb8d824410e54e459654ffac65369eb2515f3
adamshafi <adamshafi0001.com>
2026-01-18 18:25:34 +0530

    hello

---
+++ 03-spvariable.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+number1=100
+number2=300
+number3=400
+
+echo "enter first number::$number1"
+echo "enter first number::$number2"
+echo "enter first number::$number3"
+ 
+ sum=$(($number1+$number2+$number3))
+
+ echo "sum of number is::$sum"
+
+ TIMESTAMP=$(date)
+
+ echo "the scripting exected at::$TIMESTAMP"
+
+ echo "enter new password"
+
+ read -s PIN
+
+ echo "the pin is ::$PIN"
+
+ movies=("bhaubali" "raja" "rama" "ping" "singapoor")
+
+ echo "enter first movie::${movies[0]}"
+ echo "enter secondt movie::${movies[1]}"
+ echo "enter fourth movie::${movies[3]}"
+ echo "enter all movies::${movies[@]}"
+
