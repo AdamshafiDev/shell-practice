619b401a4ea55ee6dfb409ec49e83b0bb50d05f6
adamshafi <adamshafi0001.com>
2026-01-19 21:15:08 +0530

    hello

--- 04-all_test.sh
+++ 04-all_test.sh
@@ -2,6 +2,8 @@
 
 number1=$1
 number2=$2
+
+movies=("bahubali" "raja" "singamali")
 echo "enter number::$number1"
 
 echo "enter number::$number2"
@@ -9,3 +11,12 @@ echo "enter number::$number2"
 sum=$(($number1+$number2))
 
 echo "sum of number is::$sum"
+
+echo "enter pin number"
+
+read -s PIN
+
+echo "$PIN"
+
+echo "enter first movie::${movies[0]}"
+
