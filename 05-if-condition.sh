8a78d80a33bf56248eae7a2bb91fdb5a332a93a7
adamshafi <adamshafi0001.com>
2026-01-19 21:55:16 +0530

    hello

--- 05-if-condition.sh
+++ 05-if-condition.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 num=$1
 
-if [ $num -ge 100 ]
+if [ $num -gt 100 ]
  then 
   echo "the number is greater then 100"
 
-  else
+  elseif [$num -ge 100 ]
 
-  echo "the number is lessthen 100"
+  echo "the number is equel to 100"
+  
+   else
+    echo "the number is less then 100"
 
 fi
 
