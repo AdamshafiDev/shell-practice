867ead55ef0f5d962ff1ad481bef737f75d164f9
adamshafi <adamshafi0001.com>
2026-01-19 21:03:10 +0530

    hello

--- 04-all_test.sh
+++ 04-all_test.sh
@@ -1,42 +1,4 @@
 #!/bin/bash
 
-number1=100
-number2=300
-number3=400
-
-echo "enter first number::$number1"
-echo "enter first number::$number2"
-echo "enter first number::$number3"
- 
- sum=$(($number1+$number2+$number3))
-
- echo "sum of number is::$sum"
-
- TIMESTAMP=$(date)
-
- echo "the scripting exected at::$TIMESTAMP"
-
- echo "enter new password"
-
- read -s PIN
-
- echo "the pin is ::$PIN"
-
- movies=("bhaubali" "raja" "rama" "ping" "singapoor")
-
- echo "enter first movie::${movies[0]}"
- echo "enter secondt movie::${movies[1]}"
- echo "enter fourth movie::${movies[3]}"
- echo "enter all movies::${movies[*]}"
-
- echo "enter pid number::$$"
- echo "enter user name::$USER"
- echo "enter exit name::$?"
- echo "enter number name::$#"
- echo "enter user name::$PWD"
- echo "enter  name::$!"
- echo "enter user name::$1"
- echo "enter user name::$0"
-
-
-
+number=$1
+echo "enter number::$number"
