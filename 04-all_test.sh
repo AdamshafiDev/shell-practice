31813a024fa0133f4e190218a6e0c2db02e7ca0c
adamshafi <adamshafi0001.com>
2026-01-19 21:06:48 +0530

    hello

--- 04-all_test.sh
+++ 04-all_test.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-number=$1
-echo "enter number::$number"
+number1=$1
+number2=$2
+echo "enter number::$number1"
+
+echo "enter number::$number2"
+
+sum=(($number1+$number2))
+
+echo "sum of number is::$sum"
