567a13fea6f229975c48c60af49369de0d013e1f
adamshafi <adamshafi0001.com>
2026-01-20 10:32:48 +0530

    hello

--- 06-if-statement.sh
+++ 06-if-statement.sh
@@ -11,4 +11,13 @@ else
 
 fi   
 
-dnf install mysql-server -y
+dnf install mysql-server -y 
+  
+#   if [ $? -eq 0 ]
+#    then 
+#     echo "my sql is installing......SUCCESS"
+#    else
+#     echo "My SQL is installing ......FAILURE"
+#     exit 1
+#fi
+
