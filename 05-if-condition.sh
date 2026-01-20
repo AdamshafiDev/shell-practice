359364ec590fb342970885f6277fdb874599d9c7
adamshafi <adamshafi0001.com>
2026-01-20 10:12:18 +0530

    hello

--- 05-if-condition.sh
+++ 05-if-condition.sh
@@ -2,11 +2,10 @@
 num=$1
 
 if [ $num -gt 100 ]
-
-  then
-
-  echo "the number is greater then 100"
-fi
+ then
+    echo "the number is greater then 100"
+    exit 1
+     fi
 
   if [ $num -eq 100 ]
    
