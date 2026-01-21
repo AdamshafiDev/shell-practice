5ccc57ed3f28ae5a2e49f3fb988626c9a50fd941
adamshafi <adamshafi0001.com>
2026-01-21 22:38:22 +0530

    hello

--- 07-useraccess.sh
+++ 07-useraccess.sh
@@ -6,7 +6,7 @@ if [ $USERID -ne 0 ]
 then
   echo "Error: please run with root access"
   exit 1
-   echo " the scripting with root access"
+   echo "the scripting with root access"
    fi
  TIMESTAMP=($date)
    echo "the scripting running date :$TIMESTAMP"
