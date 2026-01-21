91b17660902a117a2140045c299a48cfffaca775
adamshafi <adamshafi0001.com>
2026-01-21 22:56:14 +0530

    hello

--- 07-useraccess.sh
+++ 07-useraccess.sh
@@ -10,3 +10,39 @@ then
    fi
  TIMESTAMP=$(date)
    echo "the scripting running date :$TIMESTAMP"
+
+   VALIDATE()
+   {
+     if [ $1 -ne 0 ]
+      then 
+       echo " installing:$2 .....going to insallll..SUCCESS"
+         exit 1
+       else 
+        echo " installing:$2....... failure"
+      fi
+
+   }
+  dnf list installed nginx
+   
+   if [ $? - ne 0 ]
+   then
+    echo "nginx is not install....going to install"
+     dnf install nginx -y
+     VALIDATE $? "nginx"
+    else 
+     echo "nginx is already installed ....Nothoing to do"
+    fi
+   dnf list installed mysql
+   
+   if [ $? - ne 0 ]
+   then
+    echo "mysql is not install....going to install"
+     dnf install mysql -y
+     VALIDATE $? "mysql"
+    else 
+     echo "mysql is already installed ....Nothoing to do"
+    fi
+
+
+
+
