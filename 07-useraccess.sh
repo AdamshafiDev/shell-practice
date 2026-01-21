9fcc3d11393277805da99d6db9b401edcf25d0e5
adamshafi <adamshafi0001.com>
2026-01-21 22:31:34 +0530

    hello

--- 07-useraccess.sh
+++ 07-useraccess.sh
@@ -3,11 +3,11 @@
 userid=$(id -u)
 
 if [ userid -ne 0 ]
-then
-  echo "Error"please run the script  with root access"
+ then
+  echo "Error:please run the script  with root access"
    exit 1
-   else
+else
     echo "the script running  with root acccess"
  fi
     TIMESTAMP=$(date)
-     echo "the script run date :$TIMESTAMP"
+     echo "the script run date:$TIMESTAMP"
