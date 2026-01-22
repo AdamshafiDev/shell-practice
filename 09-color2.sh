6a43f78de6621bbb8ef2018b284a751b6874ab16
adamshafi <adamshafi0001.com>
2026-01-22 09:45:27 +0530

    hello

---
+++ 09-color2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+ then
+  echo "ERROR: please run with root access"
+  exit 1
+  else
+  echo "running with roo access"
+ fi
+  
+  VALIDATE()
+    {
+        if [ $1 -eq 0 ]
+         then
+          echo "Installing..$2....Succcess"
+          else
+          echo "instealling ...$2...failure"
+         fi
+ }
+
+ dnf list installed nginx
+
+   if [ $? -ne 0 ]
+    then 
+    echo " nginix is not install ...going to installl"
+    dnf installl nginx -y
+    VALIDATE $? "nginx"
+    else
+    echo "nginx is already installed .....nothing to do"
+fi
+
+    
+     
+   
